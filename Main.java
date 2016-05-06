184b47538eae5a5f54346f3579496743f2be973c
Konrad Kaproń <kkapron@elka.pw.edu.pl>
2016-05-06 14:46:27 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+//package Main;
+
+import Controller.Controller;
+import View.View;
+import Model.Model;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Controller controller = new Controller();
+		View view = new View();
+		Model model = new Model();
+		System.out.println("Java sucks");	
+		return;
+	}
+}
